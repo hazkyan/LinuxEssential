930e05a1400acb67b9f5ce6263afcaacdd3e6933
hazkyan <hazpoldev@gmail.com>
2025-02-10 06:09:18 +0000

    Activity3

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+# Exercise01
+echo "Hello Keanu"
+
+# Exercise02
+read name
+echo "Your name is $name"
+
+#Exercise03
+echo "What is your Name?"
+read name
+echo "What is your favorite food?"
+read faveFood
+
+echo"Hello $name! I see you love $faveFood!"
